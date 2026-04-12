8801d1dfc665bc1d5d2b54f5b6e128129956c721
AyedaOk <205518917+AyedaOk@users.noreply.github.com>
2026-04-12 12:30:11 -0400

    Copying all metadata instead of a sub-set

--- RawForge.lua
+++ RawForge.lua
@@ -191,15 +191,15 @@ local function do_denoise()
     local command_exif
     if is_windows then
       command_exif = string.format(
-        'cmd /c ""%s" -overwrite_original -m -P -TagsFromFile "%s" -IFD0:Make -IFD0:Model -IFD0:UniqueCameraModel -EXIF:DateTimeOriginal -EXIF:CreateDate -EXIF:ModifyDate -EXIF:OffsetTime* -EXIF:Artist -EXIF:Copyright -EXIF:Lens* -EXIF:Focal* -EXIF:Exposure* -EXIF:ISO -XMP:all -IPTC:all -IFD0:CalibrationIlluminant1#=21 "%s""',
+        'cmd /c ""%s" -overwrite_original -TagsFromFile "%s" -all:all -IFD0:CalibrationIlluminant1#=21 "%s""',
         exif_bin, input_file, out_file
       )
     else
-      command_exif = string.format(
-        '"%s" -overwrite_original -m -P -TagsFromFile "%s" -IFD0:Make -IFD0:Model -IFD0:UniqueCameraModel -EXIF:DateTimeOriginal -EXIF:CreateDate -EXIF:ModifyDate -EXIF:OffsetTime* -EXIF:Artist -EXIF:Copyright -EXIF:Lens* -EXIF:Focal* -EXIF:Exposure* -EXIF:ISO -XMP:all -IPTC:all -IFD0:CalibrationIlluminant1#=21 "%s"',
-         exif_bin, input_file, out_file
-      )
-     end
+        command_exif = string.format(
+          '"%s" -overwrite_original -TagsFromFile "%s" -all:all -IFD0:CalibrationIlluminant1#=21 "%s"',
+          exif_bin, input_file, out_file
+        )
+    end
 
     dt.print_log("Running: " .. command_exif)
     local h2 = io.popen(command_exif)
